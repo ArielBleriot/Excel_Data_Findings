d51ed7388c1c6bcb2c522fa244763545afdc4db2
Ariel Bleriot <136377318+ArielBleriot@users.noreply.github.com>
2025-09-18 12:40:46 +0300

    Update eltweather.py

--- Airflow1_project/dags/eltweather.py
+++ Airflow1_project/dags/eltweather.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.providers.http.hooks.http import HttpHook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.decorators import task
-from airflow.utils import timezone  # Airflow 3.x replacement for days_ago
+from airflow.utils import timezone  
 import requests
 import json
 
